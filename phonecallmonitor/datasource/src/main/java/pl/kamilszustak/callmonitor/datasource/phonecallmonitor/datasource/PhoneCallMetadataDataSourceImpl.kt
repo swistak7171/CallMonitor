80faf70f4d6131c9c8596489181305ce702c42ed
Kamil Szustak <kamil.szustak98@gmail.com>
2023-09-16 13:04:14 +0200

    Fix a bug with phone call metadata not being modified

--- phonecallmonitor/datasource/src/main/java/pl/kamilszustak/callmonitor/datasource/phonecallmonitor/datasource/PhoneCallMetadataDataSourceImpl.kt
+++ phonecallmonitor/datasource/src/main/java/pl/kamilszustak/callmonitor/datasource/phonecallmonitor/datasource/PhoneCallMetadataDataSourceImpl.kt
@@ -7,20 +7,23 @@ import java.util.UUID
 
 class PhoneCallMetadataDataSourceImpl : PhoneCallMetadataDataSource {
 
-    private val metadata: MutableMap<UUID, PhoneCallMetadataDataModel> = mutableMapOf()
+    private val phoneCallMetadata: MutableMap<UUID, PhoneCallMetadataDataModel> = mutableMapOf()
 
     override suspend fun getOrCreate(id: UUID): PhoneCallMetadataDataModel {
-        return metadata.getOrPut(id) {
+        return phoneCallMetadata.getOrPut(id) {
             PhoneCallMetadataDataModel(id)
         }
     }
 
     @SuppressLint("NewApi")
     override suspend fun incrementTimesQueried(id: UUID) {
-        metadata.computeIfPresent(id) { _, metadata ->
-            metadata.copy(
-                timesQueried = metadata.timesQueried + 1
-            )
+        phoneCallMetadata.compute(id) { _, value ->
+            (value ?: PhoneCallMetadataDataModel(id))
+                .let { metadata ->
+                    metadata.copy(
+                        timesQueried = metadata.timesQueried + 1
+                    )
+                }
         }
     }
 }
