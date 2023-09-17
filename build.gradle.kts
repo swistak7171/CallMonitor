a84cf9369fad24f782bb3f8acf03fa8df99a93df
Kamil Szustak <kamil.szustak98@gmail.com>
2023-09-17 23:11:21 +0200

    Add Dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.dokka)
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+}
+
+dependencies {
+    dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:${libs.versions.dokka.get()}")
 }
