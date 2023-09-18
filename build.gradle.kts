dd5c0c78fc583d01d033d3776ac3ffd66981a755
Kamil Szustak <kamil.szustak98@gmail.com>
2023-09-18 20:42:15 +0200

    Include all components in Dokka documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.dokka.DokkaConfiguration.Visibility
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -8,6 +11,19 @@ plugins {
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
+
+    tasks.withType<DokkaTaskPartial>().configureEach {
+        dokkaSourceSets.configureEach {
+            documentedVisibilities = Visibility.values()
+                .toSet()
+
+            val readmeFileExists = file("README.md")
+                .exists()
+            if (readmeFileExists) {
+                includes.from("README.md")
+            }
+        }
+    }
 }
 
 tasks.dokkaHtmlMultiModule {
